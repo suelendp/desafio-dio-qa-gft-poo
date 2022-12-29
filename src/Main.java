17f4e6aa343bb3a5cf5bf56b26d233087c436be9
Suélen Dias Palhares <suelenddf@gmail.com>
2022-12-29 10:44:16 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,12 @@ public class Main {
         curso2.setDescricao("descrição: curso javaScript");
         curso2.setCargaHoraria(8);
 
+        Curso curso3 = new Curso();
+
+        curso3.setTitulo("Fundamentos de QA");
+        curso3.setDescricao("descrição: Fundamentos de Qualidade e Desenvolvimento de Software");
+        curso3.setCargaHoraria(10);
+
 
 
         Mentoria mentoria = new Mentoria();
@@ -40,6 +46,12 @@ public class Main {
         bootcamp.getConteudos().add(curso2);
         bootcamp.getConteudos().add(mentoria);
 
+        Bootcamp bootcampGFT = new Bootcamp();
+        bootcampGFT.setNome("GFT Quality Assurance Para Mulheres");
+        bootcampGFT.setDescricao("Descricao Bootcamp GFT QA para Mulheres");
+        bootcampGFT.getConteudos().add(curso1);
+        bootcampGFT.getConteudos().add(curso3);
+        bootcampGFT.getConteudos().add(mentoria);
 
         Dev devCamila =new Dev();
         devCamila.setNome("Camila");
@@ -69,6 +81,18 @@ public class Main {
         System.out.println("Conteudos Concluídos João: " + devJoao.getConteudosConcluidos());
         System.out.println("XP: " + devJoao.calcularTotalXp());
 
+        System.out.println("************************");
+
+
+        Dev devSuelen = new Dev();
+        devSuelen.setNome("Suélen");
+        devSuelen.inscreverBootcamp(bootcampGFT);
+        System.out.println("Conteudos Inscritos Suélen: " + devSuelen.getConteudosInscritos());
+        devSuelen.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Suélen: " + devSuelen.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos Suélen: " + devSuelen.getConteudosConcluidos());
+        System.out.println("XP: " + devSuelen.calcularTotalXp());
 
 
 
